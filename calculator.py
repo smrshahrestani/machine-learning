0d406aa2fd5ce56ce182b97c223a4ae24bde7337
S Mohammad Reza <info@shahrestani.co.uk>
2022-05-27 14:20:07 +0100

    add comments

--- calculator.py
+++ calculator.py
@@ -8,63 +8,35 @@ from joblib import PrintTime
 import numpy as np
 from regex import R, W
 
-# round 3 decimal points
-round = 3 
 
-# number of clusters
-k = 3
-
-# [(x,y), class]
-array = [
-    [(12,12), 1],
-    [(2,2), 1],
-    [(1,3), 1],
-    [(4,2), 1],
-    [(1,6), 2],
-    [(2,4), 2],
-    [(2,5), 2],
-    [(3,4), 2],
-    [(5,4), 2],
-]
-
-point = (3,1)
-
-
-
-def assignClassToPoints(array):
-    dict = {}
-    list = []
-    # for val in array:
-    #     if list.index(val[1] - 1) == -1:
-
-    #     list[val[1] - 1] = val[0]
-
-    return list
 
 
 # The Manhattan Distance between set of points to a single point
 def manhatDist(array, point):
-    allDist = []
-    print(assignClassToPoints(array))
+    print("----Manhattan Distance----")
     for i, val in enumerate(array):
         val = val[0]
         distance = abs(val[0] - point[0]) + abs(val[1] - point[1])
         distance = np.round(distance, round)
-
-        allDist.append(distance)
-
         print(f"{i}. The Manhattan distance between point {val} and point {point} is: {distance}")
+    
+    print("================================")
+    
 
 
 
 # The Euclidean Distance between set of points to a single point
 def euclidDist(array, point):
+    print("----Euclidean Distance----")
     for i, val in enumerate(array):
         val = val[0]
         distance = np.power(val[0] - point[0], 2) + np.power(val[1] - point[1], 2)
         distance = np.sqrt(distance)
         distance = np.round(distance, round)
         print(f"{i}. The Euclidean distance between point {val} and point {point} is: {distance}")
+    
+    print("================================")
+
 
 
 
@@ -103,22 +75,29 @@ def updateW1(training_points, w0, w1, learning_rate):
     return final
 
 def update_iteration(number_of_iterations, training_points, w0, w1, learning_rate):
+    print("----Batch Gradient Descend----")
     w0_new = 0
     w1_new = 0
     for i in range(number_of_iterations):
         w0_new = updateW0(training_points, w0, w1, learning_rate)
         w1_new =updateW1(training_points, w0, w1, learning_rate)
 
+        print("--------------------------------")
         print(f"iteration {i+1}: w0 = {w0_new}")
         print(f"iteration {i+1}: w1 = {w1_new}")
-        print("--------------------------------")
         w0 = w0_new
         w1 = w1_new
-
-
+    print("================================")
     return w0_new, w1_new
 
 
+
+
+# round 3 decimal points
+round = 3 
+
+
+# batch gradient descend
 training_points = [
     (1.5, 1),
     (3.5, 3),
@@ -126,14 +105,40 @@ training_points = [
     (5,3),
     (2,2.5),
 ]
-
 w0 = 0
 w1 = 0
 learning_rate = 0.01
+number_of_iterations = 3
+update_iteration(number_of_iterations,training_points,w0, w1,learning_rate)
 
-# manhatDist(array, point)
-# print("-----------------------------")
-# euclidDist(array, point)
+
+
+
+
+# Distances from a set of points to a single point
+# KNN - not ready
+
+# number of clusters
+k = 3
+
+# [(x,y), class]
+array = [
+    [(12,12), 1],
+    [(2,2), 1],
+    [(1,3), 1],
+    [(4,2), 1],
+    [(1,6), 2],
+    [(2,4), 2],
+    [(2,5), 2],
+    [(3,4), 2],
+    [(5,4), 2],
+]
+
+point = (3,1)
+
+manhatDist(array, point)
+
+euclidDist(array, point)
 
 # euclidDist([
 #     [(1,2)],
@@ -144,6 +149,6 @@ learning_rate = 0.01
 #     (1,1)
 # )
 
-# print(entropy(0.3))
 
-update_iteration(3,training_points,w0, w1,learning_rate)
+# Calculate entropy
+# print(entropy(0.3))
